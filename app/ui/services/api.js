1e2589dd5eb476ee72c0f4e2f8531b8f3b11c38c
AnnaGlushenok <annasan14052003@gmail.com>
2026-03-24 21:27:12 +0300

    changed base URL

--- app/ui/services/api.js
+++ app/ui/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://detailing-center-alpha.vercel.app/';
 
 export async function get(url) {
     const res = await fetch(BASE_URL + url);
